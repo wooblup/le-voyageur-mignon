f26ee5ca6aea5e89639a4af2bb1f99b36d08f807
Tony N <tonyxn0422@gmail.com>
2018-03-16 10:10:26 -0400

    destination route, subscribe box

---
+++ public/javascripts/main.js
@@ -0,0 +1,6 @@
+var close = document.getElementById('close');
+var subscribeBox = document.getElementById('subscribe');
+
+close.addEventListener('click', function() {
+  subscribeBox.style.display = 'none';
+});
--- routes/destination.js
+++ routes/destination.js
@@ -4,11 +4,35 @@ var router = express.Router();
 /* GET destination page. */
 router.get('/destinations/:destination', function(req, res, next) {
 
+  /* Name of city */
   var destination = req.params.destination;
+
+  /* Variable to store body text */
+  var destinationData = [];
+
+  /* Main data JSON file */
+  var dataFile = req.app.get('appData');
+
+  dataFile.destinations.forEach(function(element) {
+
+    /* Check if city name exists in data */
+    if (element.city == destination) {
+
+      destinationData.push({
+
+        city: destination,
+        photo: element.photo,
+        text: element.bodyText
+
+      });
+
+    }
+
+  });
 
   res.render('destination', {
     title: 'Le Voyageur Mignonne',
-    city: destination
+    destinationData: destinationData
   });
 
 });
